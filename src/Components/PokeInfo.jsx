bf5d97349bc7bc8732da006fe4c55c7fedfa4976
vaperLee <rize1126jesse@gmail.com>
2023-01-17 21:53:21 +0900

    first step done

--- src/Components/PokeInfo.jsx
+++ src/Components/PokeInfo.jsx
@@ -1,30 +1,42 @@
-import react from "react";
+import React from "react";
 
-const PokeInfo = () => {
+const Pokeinfo = ({ data }) => {
   return (
     <>
-      <h1>リザードン</h1>
-      <img
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png"
-        alt=""
-      />
-      <div className="abilities">
-        <div className="group">
-          <h2>blaze</h2>
-        </div>
-        <div className="group">
-          <h2>solar-power</h2>
-        </div>
-        <div className="base-stat">
-          <h3>HP:30</h3>
-          <h3>HP:30</h3>
-          <h3>HP:30</h3>
-          <h3>HP:30</h3>
-          <h3>HP:30</h3>
-        </div>
-      </div>
+      {!data ? (
+        ""
+      ) : (
+        <>
+          <h1>{data.name}</h1>
+          <img
+            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${data.id}.svg`}
+            alt=""
+          />
+          <div className="abilities">
+            {data.abilities.map((poke) => {
+              return (
+                <>
+                  <div className="group">
+                    <h2>{poke.ability.name}</h2>
+                  </div>
+                </>
+              );
+            })}
+          </div>
+          <div className="base-stat">
+            {data.stats.map((poke) => {
+              return (
+                <>
+                  <h3>
+                    {poke.stat.name}:{poke.base_stat}
+                  </h3>
+                </>
+              );
+            })}
+          </div>
+        </>
+      )}
     </>
   );
 };
-
-export default PokeInfo;
+export default Pokeinfo;
